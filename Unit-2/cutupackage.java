b1783364acf39a71eb7af04a5ca7d38930f37c3d
MimansaPatle <mimansapatle@gmail.com>
2025-03-20 11:16:50 +0530

    Add files via upload

---
+++ Unit-2/cutupackage.java
@@ -0,0 +1,10 @@
+import mycutepackage.package2; 
+public class cutupackage { 
+public static void main(String[] args) { 
+//package1 obj = new package1(); 
+//obj.display(); 
+package2 obj = new package2();
+obj.mini();
+
+} 
+}
